f9622fe65710fc3f8b61d164cba2d6a74cf7d4c3
jaegerwang <jaegerwang@tencent.com>
2021-07-14 21:55:01 +0800

    all: init_framework.
    引入 ant-design-vue.
    更新 router:
     - / => index
     - /user => user page
    初始化 `app.vue`, 页面分为`topbar`、`sidebar`、`content`三部分
    - `topbar`, `sidebar` 在 `components` 目录下完成
    - `content` 为主视图，使用router, 具体页面在`view` 目录下完成

---
+++ src/components/TopBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <main>
+        <a-icon
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="changeMenu(collapsed)"
+        />
+    </main>
+</template>
+
+<script>
+
+export default {
+    model: {
+        prop: 'collapsed',
+        event: 'changeMenu'
+    },
+    props: {
+        // close or not.
+        collapsed: Boolean
+    },
+    methods: {
+        changeMenu(collapsed) {
+            this.$emit('changeMenu', !collapsed)
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+.trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+.trigger:hover {
+  color: #1890ff;
+}
+
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,30 @@
+<template>
+    <main>
+        <a-card title="个人信息" class="card_item"> 
+            TODO(tangzhenlin): 实现个人信息组件.
+        </a-card>
+        <a-card title="订单列表" class="card_item"> 
+            TODO(zhaoyingkai): 实现订单组件.
+        </a-card>
+        <a-card title="购物车" class="card_item"> 
+            TODO(wangsaiyu): 实现购物车组件.
+        </a-card>
+    </main>
+</template>
+
+
+<script>
+
+export default {
+    
+}
+
+</script>
+
+<style scoped>
+
+.card_item {
+    margin-bottom: 2vh;
+}
+
+</style>
