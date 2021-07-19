0db12c1aa8f8d6d64140d376dfbfd22032507158
tangzl7 <3155837867@qq.com>
2021-07-19 12:13:29 +0800

    重新提交一下

---
+++ src/views/LoginRegister.vue
@@ -0,0 +1,16 @@
+<template>
+  <div align="center" style="margin-top: 3%">
+    <img src="../assets/logo.png" alt="">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
