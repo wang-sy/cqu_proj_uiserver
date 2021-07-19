e8345f4a3d4cd66e12090e9856a1fc0ceae32a6e
IFrostI <1486255445@qq.com>
2021-07-19 15:08:02 +0800

    更新订单列表代码

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -1,75 +1,73 @@
 <template>
   <a-card title="订单列表">
-    <div>
-      <a-collapse v-model="activeKey">
-        <a-collapse-panel key="1" header="未完成的订单">
-          <a-list
-          class="Order_list"
-          :loading="loading"
-          item-layout="horizontal"
-          :data-source="data1"
+    <a-collapse v-model="activeKey">
+      <a-collapse-panel key="1" header="未完成的订单">
+        <a-list
+        class="Order_List"
+        :loading="loading"
+        item-layout="horizontal"
+        :data-source="list_completed"
+        >
+          <div
+            v-if="showLoadingMore"
+            slot="loadMore"
+            :style="{ textAlign: 'center', marginTop: '12px', height: '32px', lineHeight: '32px' }"
           >
-            <div
-              v-if="showLoadingMore"
-              slot="loadMore"
-              :style="{ textAlign: 'center', marginTop: '12px', height: '32px', lineHeight: '32px' }"
+            <a-spin v-if="loadingMore" />
+            <a-button v-else @click="onLoadMore_list_completed">
+              加载更多
+            </a-button>
+          </div>
+          <a-list-item slot="renderItem" slot-scope="item, index">
+          <!-- <a-list-item slot="renderItem" slot-scope="item">   -->
+            <a slot="actions" href="https://www.baidu.com/">查看订单</a>
+            <a slot="actions" @click="delete_list_completed(index)">删除订单</a>
+            <a-list-item-meta
+              :description="'描述:'+item.description+' 单价:￥'+item.price+' 数量:'+item.number+' 总价格:￥'+item.totalPrice"
             >
-              <a-spin v-if="loadingMore" />
-              <a-button v-else @click="onLoadMore1">
-                加载更多
-              </a-button>
-            </div>
-            <a-list-item slot="renderItem" slot-scope="item, index">
-            <!-- <a-list-item slot="renderItem" slot-scope="item">   -->
-              <a slot="actions" href="https://www.baidu.com/">查看订单</a>
-              <a slot="actions" @click="deleteItem1(index)">删除订单</a>
-              <a-list-item-meta
-                :description="'描述:'+item.description+' 单价:￥'+item.price+' 数量:'+item.number+' 总价格:￥'+item.totalPrice"
-              >
-                <a slot="title" href="https://www.antdv.com/">{{ item.name }}</a>
-              </a-list-item-meta>
-              <div class="word_color">进行中</div>
-            </a-list-item>
-          </a-list>
-        </a-collapse-panel>
-        <a-collapse-panel key="2" header="已完成的订单" :disabled="false">
-          <a-list
-          class="Order_list"
-          :loading="loading"
-          item-layout="horizontal"
-          :data-source="data2"
+              <a slot="title" href="https://www.antdv.com/">{{ item.name }}</a>
+            </a-list-item-meta>
+            <div class="word_color">进行中</div>
+          </a-list-item>
+        </a-list>
+      </a-collapse-panel>
+      <a-collapse-panel key="2" header="已完成的订单" :disabled="false">
+        <a-list
+        class="Order_list"
+        :loading="loading"
+        item-layout="horizontal"
+        :data-source="list_incompleted"
+        >
+          <div
+            v-if="showLoadingMore"
+            slot="loadMore"
+            :style="{ textAlign: 'center', marginTop: '12px', height: '32px', lineHeight: '32px' }"
           >
-            <div
-              v-if="showLoadingMore"
-              slot="loadMore"
-              :style="{ textAlign: 'center', marginTop: '12px', height: '32px', lineHeight: '32px' }"
+            <a-spin v-if="loadingMore" />
+            <a-button v-else @click="onLoadMore_list_incompleted">
+              加载更多
+            </a-button>
+          </div>
+          <a-list-item slot="renderItem" slot-scope="item, index">
+          <!-- <a-list-item slot="renderItem" slot-scope="item">   -->
+            <a slot="actions" href="https://www.baidu.com/">查看订单</a>
+            <a slot="actions" @click="delete_list_incompleted(index)">删除订单</a>
+            <a-list-item-meta
+              :description="'描述:'+item.description+' 单价:￥'+item.price+' 数量:'+item.number+' 总价格:￥'+item.totalPrice"
             >
-              <a-spin v-if="loadingMore" />
-              <a-button v-else @click="onLoadMore2">
-                加载更多
-              </a-button>
-            </div>
-            <a-list-item slot="renderItem" slot-scope="item, index">
-            <!-- <a-list-item slot="renderItem" slot-scope="item">   -->
-              <a slot="actions" href="https://www.baidu.com/">查看订单</a>
-              <a slot="actions" @click="deleteItem2(index)">删除订单</a>
-              <a-list-item-meta
-                :description="'描述:'+item.description+' 单价:￥'+item.price+' 数量:'+item.number+' 总价格:￥'+item.totalPrice"
-              >
-                <a slot="title" href="https://www.antdv.com/">{{ item.name }}</a>
-              </a-list-item-meta>
-              <div>已完成</div>
-            </a-list-item>
-          </a-list>
-        </a-collapse-panel>
-      </a-collapse>
-    </div>
+              <a slot="title" href="https://www.antdv.com/">{{ item.name }}</a>
+            </a-list-item-meta>
+            <div>已完成</div>
+          </a-list-item>
+        </a-list>
+      </a-collapse-panel>
+    </a-collapse>
   </a-card>
 </template>
 
 <script>
 
-let data1 = [
+let list_completed = [
   {
     name: '七彩虹吉列威锋竞速3070显卡（吃鸡英雄联盟绝地求生）',
     description:"Fuck and Shit",
@@ -92,7 +90,7 @@ let data1 = [
     totalPrice: 475
   }
 ]
-let data2 = [
+let list_incompleted = [
   {
     name: '七彩虹吉列威锋竞速3070显卡（吃鸡英雄联盟绝地求生）',
     description:"Fuck and Shit",
@@ -122,33 +120,33 @@ export default {
       loading: false,
       loadingMore: false,
       showLoadingMore: true,
-      data1: data1,
-      data2: data2
+      list_completed: list_completed,
+      list_incompleted: list_incompleted
     };
   },
   methods: {
-    onLoadMore1() {
+    onLoadMore_list_completed() {
       this.loadingMore = true;
-      this.data1 = this.data1.concat(data1);
+      this.list_completed = this.list_completed.concat(list_completed);
       this.loadingMore = false;
     },
-    onLoadMore2() {
+    onLoadMore_list_incompleted() {
       this.loadingMore = true;
-      this.data2 = this.data2.concat(data1);
+      this.list_incompleted = this.list_incompleted.concat(list_incompleted);
       this.loadingMore = false;
     },
-    deleteItem1(index){
-      this.data1.splice(index,1)
+    delete_list_completed(index){
+      this.list_completed.splice(index,1)
     },
-    deleteItem2(index){
-      this.data2.splice(index,1)
+    delete_list_incompleted(index){
+      this.list_incompleted.splice(index,1)
     }
   },
 };
 </script>
 
 <style>
-.Order-list {
+.Order-List {
   min-height: 350px;
 }
 .word_color{
