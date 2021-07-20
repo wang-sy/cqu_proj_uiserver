5fbfec6cc49ef9e0aa3a31e5f73dc6b29c8a9ea5
jaegerwang <46556407+wang-sy@users.noreply.github.com>
2021-07-20 15:24:19 +0800

    impl index page. (#5)
    * impl index page.
    
    * fix: style.

---
+++ src/components/CategoryView.vue
@@ -0,0 +1,55 @@
+<template>
+    <main>
+        <b class="Title">{{title}}</b>
+        <!-- todo(wangsaiyu): add this.$router.push(productList, categoryID=id) -->
+        <a class="MoreButton">更多商品</a>
+        <a-divider />
+        <!-- good list -->
+        <a-row :gutter="[32,32]">
+            <a-col v-for="(item, index) in goods" :key="index" :span="6">
+                <GoodView
+                    :id="item.id"
+                    :name="item.name"
+                    :description="item.description"
+                    :figure="item.figure"
+                    :price="item.price"
+                />
+            </a-col>
+        </a-row>
+    </main>
+</template>
+
+<script>
+import GoodView from '../components/GoodView'
+
+export default {
+    components: {
+        GoodView
+    },
+    props: {
+        title: String,
+        categoryID: Number,
+        goods: Array
+    },
+}
+
+</script>
+
+
+<style scoped>
+.Title {
+    font-size: 1.5em;
+    float: left;
+}
+
+.MoreButton {
+    font-size: 1em;
+    float: right;
+}
+
+.GoodListItem {
+    width: 18%;
+    margin-right: 2%;
+}
+
+</style>
---
+++ src/components/GoodView.vue
@@ -0,0 +1,37 @@
+<template>
+    <a-card hoverable>
+        <img
+            slot="cover"
+            alt="example"
+            :src="figure"
+        />
+        <a-card-meta :title="name">
+            <template slot="description">
+                {{description}}
+                <a-row>
+                    <a-col :span="12" style="margin-top: 8px"><b>{{price}} ¥</b></a-col>
+                    <a-col :span="12">
+                        <a-button type="dashed" style="width: 100%">立刻购买</a-button>
+                    </a-col>
+                </a-row>
+            </template>
+        </a-card-meta>
+    </a-card>
+</template>
+
+<script>
+export default {
+    props: {
+        id: Number,
+        name: String,
+        description: String,
+        figure: String,
+        price: Number
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
