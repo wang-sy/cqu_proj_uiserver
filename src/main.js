ec06952f9c344c629156d2fd1d4ccb9d564f77d6
jaegerwang <46556407+wang-sy@users.noreply.github.com>
2021-07-22 21:23:55 +0800

    impl shoppingCart by localStorage. (#23)

--- src/main.js
+++ src/main.js
@@ -15,6 +15,21 @@ Vue.prototype.$submitOrder = function (goodsList) {
   EventBus.$emit('submitOrder', goodsList)
 }
 
+Vue.prototype.$setShoppingCart = function (shoppingCart) {
+  const rawShoppingCartInfo = JSON.stringify(shoppingCart)
+
+  localStorage.setItem("teapotShopppingCart", rawShoppingCartInfo)
+}
+
+Vue.prototype.$getShoppingCart = function () {
+  const rawShoppingCartInfo = localStorage.getItem("teapotShopppingCart")
+  if (rawShoppingCartInfo === undefined || rawShoppingCartInfo === null) {
+    return []
+  }
+
+  return JSON.parse(rawShoppingCartInfo)
+}
+
 new Vue({
   router,
   render: h => h(App)
