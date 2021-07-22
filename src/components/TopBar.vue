e24b43d41ebe286d408dc5ebee15fe71ece71225
tangzl7 <3155837867@qq.com>
2021-07-22 23:17:54 +0800

    delete my option in topbar

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -6,19 +6,18 @@
         </a-layout-sider>
         <a-layout-content style="margin-top: 0.5%; margin-left: 4%">
          <a-menu mode="horizontal">
-           <a-menu-item key="1" @click="to_user('/user')" style="font-size: 16px">我的</a-menu-item>
-           <a-menu-item key="2" @click="to_goods_list_page('vga')" style="font-size: 16px">显卡</a-menu-item>
-           <a-menu-item key="3" @click="to_goods_list_page('memory')" style="font-size: 16px">内存</a-menu-item>
-           <a-menu-item key="4" @click="to_goods_list_page('motherboard')" style="font-size: 16px">主板</a-menu-item>
-           <a-menu-item key="5" @click="to_goods_list_page('cpu')" style="font-size: 16px">处理器</a-menu-item>
-           <a-menu-item key="6" @click="to_goods_list_page('hard_drives')" style="font-size: 16px">硬盘</a-menu-item>
-           <a-menu-item key="7" @click="to_goods_list_page('case')" style="font-size: 16px">机箱</a-menu-item>
-           <a-menu-item key="8" @click="to_goods_list_page('power')" style="font-size: 16px">电源</a-menu-item>
+           <a-menu-item key="1" @click="to_goods_list_page('vga')" style="font-size: 16px">显卡</a-menu-item>
+           <a-menu-item key="2" @click="to_goods_list_page('memory')" style="font-size: 16px">内存</a-menu-item>
+           <a-menu-item key="3" @click="to_goods_list_page('motherboard')" style="font-size: 16px">主板</a-menu-item>
+           <a-menu-item key="4" @click="to_goods_list_page('cpu')" style="font-size: 16px">处理器</a-menu-item>
+           <a-menu-item key="5" @click="to_goods_list_page('hard_drives')" style="font-size: 16px">硬盘</a-menu-item>
+           <a-menu-item key="6" @click="to_goods_list_page('case')" style="font-size: 16px">机箱</a-menu-item>
+           <a-menu-item key="7" @click="to_goods_list_page('power')" style="font-size: 16px">电源</a-menu-item>
             <a-popover style="margin-left: 27%; font-size: 16px">
               <template slot="content">
                 <p>你好：{{username}}</p>
               </template>
-              <a-avatar :src="avatar" style="width: 3.5%; height: 3.5%; margin-left: 50%; margin-bottom: 1%"/>
+              <a-avatar :src="avatar" style="width: 3.5%; height: 3.5%; margin-left: 50%; margin-bottom: 1%" @click="to_user('/user')"/>
             </a-popover>
            <a-input-search placeholder="输入搜索内容" style="width: 15%; margin-left: 1%"
                                         @search="to_search('/goodslist', search_content)" v-model="search_content"/>
